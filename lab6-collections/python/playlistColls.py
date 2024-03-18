9a8c486439c854bacaa3118e507de4cb6653f66b
pablofelguera <157590400+pablofelguera@users.noreply.github.com>
2024-03-18 10:57:54 +0000

    Update playlistColls.py

--- lab6-collections/python/playlistColls.py
+++ lab6-collections/python/playlistColls.py
@@ -74,7 +74,7 @@ playlist1noAds = [ item for item in playlist1 if item.__class__ != Advert ]
 
 print("playlist1noAds = %s" % playlist1noAds)
 
-shortItemLenghts1 = ["todo"] # TASK 6.3.(b) -- replace "todo" by a list comprehension
+shortItemLenghts1 = [item.length_secs for item in playlist1 if item.length_secs <20] # TASK 6.3.(b) -- replace "todo" by a list comprehension
 
 print("shortItemLenghts1 = %s" % shortItemLenghts1)
 
